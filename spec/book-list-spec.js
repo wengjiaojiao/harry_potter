e8216479a0f2d6949df41bfb470cd28e317dd610
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-09 19:11:42 +0800

    add the field count of booklist in the spec file

--- spec/book-list-spec.js
+++ spec/book-list-spec.js
@@ -2,11 +2,12 @@
 
 var BookList = require('../src/book-list.js');
 
-describe('Book',function() {
+describe('BookList',function() {
     it("should have field chapter and price", function() {
-        var bookList = new BookList(1);
+        var bookList = new BookList(1,2);
 
         expect(bookList.chapter).toBe(1);
+        expect(bookList.count).toBe(2);
         expect(bookList.price).toBe(8);
     });
 });
