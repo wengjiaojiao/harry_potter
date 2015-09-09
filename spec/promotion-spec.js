d3e6762e21cf9bf3677f23a49b0a7c2f153ee843
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-09 19:12:30 +0800

    create the promotion spec file

---
+++ spec/promotion-spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var Promotion = require('../src/promotion.js');
+
+describe('Promotion',function() {
+    it("should have method differentPromotion, one different book have no promotion", function() {
+        var promotion = new Promotion();
+
+        expect(promotion.differentPromotion([[1]])).toBe(8);
+    });
+
+    it("should have method differentPromotion, two different book have 5% promotion", function() {
+        var promotion = new Promotion();
+
+        expect(promotion.differentPromotion([[1,2]])).toBe(15.2);
+    });
+
+
+    it("should have method differentPromotion, three different book have 10% promotion", function() {
+        var promotion = new Promotion();
+
+        expect(promotion.differentPromotion([[1,2,3]])).toBe(21.6);
+    });
+
+    it("should have method differentPromotion, four different book have 20% promotion", function() {
+        var promotion = new Promotion();
+
+        expect(promotion.differentPromotion([[1,2,3,4]])).toBe(25.6);
+    });
+
+    it("should have method differentPromotion, five different book have 25% promotion", function() {
+        var promotion = new Promotion();
+
+        expect(promotion.differentPromotion([[1,2,3,4,5]])).toBe(30);
+    });
+
+    it("should have method differentPromotion, different group have own promotion", function() {
+        var promotion = new Promotion();
+
+        expect(promotion.differentPromotion([[1,2,3],[2,3]])).toBe(36.8);
+    });
+});
