7457ff774668aaad5d0323d7df93538934aab381
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-11 17:24:41 +0800

    remove the bookPrice as the Promotion model field

--- src/promotion.js
+++ src/promotion.js
@@ -1,26 +1,27 @@
-function Promotion(){
+function Promotion(bookPrice) {
+    this.bookPrice = bookPrice;
 }
 
 Promotion.prototype.differentPromotion = function(basket) {
-    var bookDiscount = [0.95,0.9,0.8,0.75];
-    var bookCount = [2,3,4,5];
-    var bookPrice = 8;
+    var bookDiscount = [0.95, 0.9, 0.8, 0.75];
+    var bookCount = [2, 3, 4, 5];
     var total = 0;
+    var that = this;
 
     basket.forEach(function(val, i) {
         var isOne = true;
 
-        bookCount.forEach(function(count,j) {
-            if(val.length === count) {
-                total += bookPrice * count * bookDiscount[j];
+        bookCount.forEach(function(count, j) {
+            if (val.length === count) {
+                total += that.bookPrice * count * bookDiscount[j];
                 isOne = false;
             }
         });
         if (isOne) {
-            total += bookPrice;
+            total += that.bookPrice;
         }
     });
     return total;
-}
+};
 
 module.exports = Promotion;
