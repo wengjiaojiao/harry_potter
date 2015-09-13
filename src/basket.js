66417ad2f4e05a8eea91cdfc73a63a81254c8ccc
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-13 16:06:37 +0800

    create the basket model file

---
+++ src/basket.js
@@ -0,0 +1,11 @@
+var _ = require('../node_modules/lodash-3.9.0-npm');
+
+function Basket(){
+    this.basket = [];
+}
+
+Basket.prototype.group = function(bookList) {
+
+};
+
+module.exports = Basket;
