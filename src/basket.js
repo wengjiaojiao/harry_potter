d528427ebeb021590cd369d5406f624914f8a465
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-14 10:42:43 +0800

    get the max count and according this to gropu

--- src/basket.js
+++ src/basket.js
@@ -4,8 +4,31 @@ function Basket(){
     this.basket = [];
 }
 
-Basket.prototype.group = function(bookList) {
+Basket.prototype.getMaxCount = function(bookList) {
+    var max;
 
+    max = _.max(bookList, function (book) {
+        return book;
+    });
+    return max;
+};
+
+Basket.prototype.group = function(bookList) {
+    var that = this;
+    var maxCount = this.getMaxCount(bookList);
+
+    while(maxCount != 0){
+        var subArray = [];
+
+        for (var k in bookList) {
+            if(bookList[k] !=0) {
+                subArray.push (Number(k));
+                bookList[k]--;
+            }
+        }
+        maxCount --;
+        that.basket.push(subArray);
+    }
 };
 
 module.exports = Basket;
