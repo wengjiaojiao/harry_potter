1c3a130c174aefc00232f1788e12112843243027
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-14 10:44:17 +0800

    add the spec about the special promotion method

--- spec/promotion-spec.js
+++ spec/promotion-spec.js
@@ -3,40 +3,51 @@
 var Promotion = require('../src/promotion.js');
 
 describe('Promotion',function() {
-    it("should have method differentPromotion, one different book have no promotion", function() {
-        var promotion = new Promotion(8);
+    describe('#differentPromotion',function() {
+        it("one different book have no promotion", function() {
+            var promotion = new Promotion(8);
 
-        expect(promotion.differentPromotion([[1]])).toBe(8);
+            expect(promotion.differentPromotion([[1]])).toBe(8);
+        });
+
+        it("two different book have 5% promotion", function() {
+            var promotion = new Promotion(8);
+
+            expect(promotion.differentPromotion([[1,2]])).toBe(15.2);
+        });
+
+
+        it("three different book have 10% promotion", function() {
+            var promotion = new Promotion(8);
+
+            expect(promotion.differentPromotion([[1,2,3]])).toBe(21.6);
+        });
+
+        it("four different book have 20% promotion", function() {
+            var promotion = new Promotion(8);
+
+            expect(promotion.differentPromotion([[1,2,3,4]])).toBe(25.6);
+        });
+
+        it("five different book have 25% promotion", function() {
+            var promotion = new Promotion(8);
+
+            expect(promotion.differentPromotion([[1,2,3,4,5]])).toBe(30);
+        });
+
+        it("different group have own promotion", function() {
+            var promotion = new Promotion(8);
+
+            expect(promotion.differentPromotion([[1,2,3],[2,3]])).toBe(36.8);
+        });
+
     });
 
-    it("should have method differentPromotion, two different book have 5% promotion", function() {
-        var promotion = new Promotion(8);
+    describe('#special',function(){
+        it("should cheaper 0.4 when have the special situation", function() {
+            var promotion = new Promotion(8);
 
-        expect(promotion.differentPromotion([[1,2]])).toBe(15.2);
-    });
-
-
-    it("should have method differentPromotion, three different book have 10% promotion", function() {
-        var promotion = new Promotion(8);
-
-        expect(promotion.differentPromotion([[1,2,3]])).toBe(21.6);
-    });
-
-    it("should have method differentPromotion, four different book have 20% promotion", function() {
-        var promotion = new Promotion(8);
-
-        expect(promotion.differentPromotion([[1,2,3,4]])).toBe(25.6);
-    });
-
-    it("should have method differentPromotion, five different book have 25% promotion", function() {
-        var promotion = new Promotion(8);
-
-        expect(promotion.differentPromotion([[1,2,3,4,5]])).toBe(30);
-    });
-
-    it("should have method differentPromotion, different group have own promotion", function() {
-        var promotion = new Promotion(8);
-
-        expect(promotion.differentPromotion([[1,2,3],[2,3]])).toBe(36.8);
+            expect(promotion.specialPromotion([[1,2,3],[2,3]])).toBe(36.4);
+        });
     });
 });
