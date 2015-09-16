fc8cd93c84811b353e5c0090bc4d0f145e9dd0df
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-16 16:42:46 +0800

    change the book filed and modify the test

--- spec/book-spec.js
+++ spec/book-spec.js
@@ -4,9 +4,8 @@ var Book = require('../src/book.js');
 
 describe('Book', function() {
     it('should hava filed chapter and count', function() {
-        var book = new Book(1,2);
+        var book = new Book(1);
 
         expect(book.chapter).toBe(1);
-        expect(book.count).toBe(2);
     });
 });
