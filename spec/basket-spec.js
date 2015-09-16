341285f4dcd690cdccdb8f6ca6ea877e782f2eb3
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-16 15:00:45 +0800

    test the private getMaxCount method

--- spec/basket-spec.js
+++ spec/basket-spec.js
@@ -3,20 +3,18 @@
 var Basket = require('../src/basket.js');
 
 describe('Basket',function() {
-    describe('#getMaxCount', function () {
+    describe('.getMaxCount(bookList)', function () {
         it('should get the max count in the bookList', function() {
-            var basket = new Basket();
-            var result = basket.getMaxCount({1:2,2:2,3:1});
+            var result = Basket.getMaxCount({1:2,2:2,3:1});
 
             expect(result).toBe(2);
         });
     });
 
-    describe('#group',function() {
+    describe('#group(bookList)',function() {
         it("should group the different books", function() {
             var basket = new Basket();
 
-            basket.getMaxCount({1:2,2:2,3:1});
             basket.group({1:2,2:2,3:1});
             expect(basket.basket).toEqual([[1,2,3],[1,2]]);
         });
