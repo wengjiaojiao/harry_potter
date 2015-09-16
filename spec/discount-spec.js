59c6e7e2d12d339dfa6c9f5195bfd0cf8b72270f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-16 17:07:18 +0800

    remove the filed price

--- spec/discount-spec.js
+++ spec/discount-spec.js
@@ -1,38 +1,44 @@
 'use strict';
 
 var Discount = require('../src/discount.js');
+var Book = require('../src/book.js');
 
 describe('Discount',function() {
     describe('#discountRule',function() {
         it("one different book have no discount", function() {
-            var discount = new Discount(8);
+            var discount = new Discount();
+            var book = new Book(1);
 
-            expect(discount.discountRule([[1]])[0]).toBe(8);
+            expect(discount.discountRule([[1]],book)[0]).toBe(8);
         });
 
         it("two different book have 5% discount", function() {
-            var discount = new Discount(8);
+            var discount = new Discount();
+            var book = new Book(1);
 
-            expect(discount.discountRule([[1,2]])[0]).toBe(7.6);
+            expect(discount.discountRule([[1,2]], book)[0]).toBe(7.6);
         });
 
 
         it("three different book have 10% discount", function() {
-            var discount = new Discount(8);
+            var discount = new Discount();
+            var book = new Book(1);
 
-            expect(discount.discountRule([[1,2,3]])[0]).toBe(7.2);
+            expect(discount.discountRule([[1,2,3]], book)[0]).toBe(7.2);
         });
 
         it("four different book have 20% discount", function() {
-            var discount = new Discount(8);
+            var discount = new Discount();
+            var book = new Book(1);
 
-            expect(discount.discountRule([[1,2,3,4]])[0]).toBe(6.4);
+            expect(discount.discountRule([[1,2,3,4]], book)[0]).toBe(6.4);
         });
 
         it("five different book have 25% discount", function() {
-            var discount = new Discount(8);
+            var discount = new Discount();
+            var book = new Book(1);
 
-            expect(discount.discountRule([[1,2,3,4,5]])[0]).toBe(6);
+            expect(discount.discountRule([[1,2,3,4,5]], book)[0]).toBe(6);
         });
     });
 });
--- src/discount.js
+++ src/discount.js
@@ -1,10 +1,9 @@
-function Discount(bookPrice) {
-    this.bookPrice = bookPrice;
+function Discount() {
     this.bookDiscount = [1, 0.95, 0.9, 0.8, 0.75];
     this.bookCount = [1, 2, 3, 4, 5];
 }
 
-Discount.prototype.discountRule = function(basket) {
+Discount.prototype.discountRule = function(basket, book) {
     var that = this;
     var promotionPrice = 0;
     var promotionPrices = [];
@@ -12,7 +11,7 @@ Discount.prototype.discountRule = function(basket) {
     basket.forEach(function(val, i) {
         that.bookCount.forEach(function(count, j) {
             if(val.length === count) {
-                promotionPrice = that.bookPrice * that.bookDiscount[j];
+                promotionPrice = book.price * that.bookDiscount[j];
             }
         });
         promotionPrices.push(promotionPrice);
