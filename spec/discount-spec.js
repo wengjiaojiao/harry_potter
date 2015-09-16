d8f65d7263eb65d42272cd202a292dc213b56f27
wengjiaojiao <wengjiaojiao@outlook.com>
2015-09-16 14:49:35 +0800

    create the spec file about the discount

---
+++ spec/discount-spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Discount = require('../src/discount.js');
+
+describe('Discount',function() {
+    describe('#discountRule',function() {
+        it("one different book have no discount", function() {
+            var discount = new Discount(8);
+
+            expect(discount.discountRule([[1]])[0]).toBe(8);
+        });
+
+        it("two different book have 5% discount", function() {
+            var discount = new Discount(8);
+
+            expect(discount.discountRule([[1,2]])[0]).toBe(7.6);
+        });
+
+
+        it("three different book have 10% discount", function() {
+            var discount = new Discount(8);
+
+            expect(discount.discountRule([[1,2,3]])[0]).toBe(7.2);
+        });
+
+        it("four different book have 20% discount", function() {
+            var discount = new Discount(8);
+
+            expect(discount.discountRule([[1,2,3,4]])[0]).toBe(6.4);
+        });
+
+        it("five different book have 25% discount", function() {
+            var discount = new Discount(8);
+
+            expect(discount.discountRule([[1,2,3,4,5]])[0]).toBe(6);
+        });
+    });
+});
